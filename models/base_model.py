052cc991373b6cc156d91878396ae95e8f46d4b0
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-06 17:57:48 +0100

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -14,10 +14,19 @@ class BaseModel:
     This is the base class...more details to be added later
     """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+
+        if **kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ["created_at", "updated_at"]:
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -37,7 +46,7 @@ class BaseModel:
         """
         Returns a dictionary containing all keys/values of the instance
         """
-        dictionary = self.__dict__
+        dictionary = self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
         dictionary['created_at'] = datetime.isoformat(self.created_at)
         dictionary['updated_at'] = datetime.isoformat(self.updated_at)
