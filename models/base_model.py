b45c32bb84efc8e2c3075edec9cdc9688b6ce6df
kiplimoboor <kiplimoboor@pm.me>
2023-12-06 15:35:15 +0300

    base model first creation

--- models/base_model.py
+++ models/base_model.py
@@ -19,8 +19,27 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-    def updated_time(self):
-        self.updated_at = datetime.now()
-    
     def __str__(self):
+        """
+        Returns the string representation of the instance
+        """
+
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        Updates the public instance attribute updated_at with current datetime
+        """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of the instance
+        """
+        dictionary = self.__dict__
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = datetime.isoformat(self.created_at)
+        dictionary['updated_at'] = datetime.isoformat(self.updated_at)
+
+        return dictionary
