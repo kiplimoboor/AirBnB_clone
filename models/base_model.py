ff154a593ede2e7e47e64805736d0e49a5add65c
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-06 12:31:48 +0100

    basemodel

---
+++ models/base_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines a class Basemodel that defines all common attributes
+ and methods from other classes
+"""
+
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    This is the base class...more details to be added later
+    """
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def updated_time(self):
+        self.updated_at = datetime.now()
+
