1f688792eaac6a75f1781c2bb37168ddd2aee79f
kiplimoboor <kiplimoboor@pm.me>
2023-12-08 10:48:55 +0300

    create command

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 This module containes the CLI implementation for the project
 """
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -22,7 +23,28 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
+        """
+        Skip an empty line
+        """
         pass
+
+    def do_create(self, arg):
+        """
+        Usage: create BaseModel
+        Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id.
+        """
+
+        if not arg:
+            print("** class name missing **")
+            return
+        if arg != 'BaseModel':
+            print("** class doesn't exist **")
+            return
+
+        new_model = BaseModel()
+        new_model.save()
+        print(new_model.id)
 
 
 if __name__ == '__main__':
