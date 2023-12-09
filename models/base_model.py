fd2bbe02a1207e1f88be98ce0331a861756d4b52
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-09 22:06:03 +0100

    update to base model

--- models/base_model.py
+++ models/base_model.py
@@ -5,18 +5,26 @@ This module defines a class Basemodel that defines all common attributes
 """
 
 
+import models
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
     """
-    This is the base class...more details to be added later
+    This is the base model of the airbnb clone project.
+
+    Properties:
+        *args: not used
+        **kwargs: key/value pairs of attributes
     """
 
     def __init__(self, *args, **kwargs):
+        """Initializes a new BaseModel"""
 
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -25,10 +33,6 @@ class BaseModel:
                     else:
                         setattr(self, str(key), value)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
             storage.new(self.to_dict())
 
     def __str__(self):
@@ -44,7 +48,7 @@ class BaseModel:
         """
 
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
@@ -52,7 +56,7 @@ class BaseModel:
         """
         dictionary = self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = datetime.isoformat(self.created_at)
-        dictionary['updated_at'] = datetime.isoformat(self.updated_at)
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
 
         return dictionary
