38d414cc2e42ffa7f593e71e127b93adc2e94980
ORVILLE <61908262+kiplimoboor@users.noreply.github.com>
2023-12-09 23:25:04 +0300

    all

--- console.py
+++ console.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-This module contains the CLI implementation for the project
+This module containes the CLI implementation for the project
 """
 import cmd
 import json
+import re
 from models.base_model import BaseModel
 from models import storage
 
@@ -56,12 +57,12 @@ class HBNBCommand(cmd.Cmd):
             saved_models = storage.all()
             model = find_model(saved_models, id)
             if model:
-                print(saved_models[model])
+                print(BaseModel(**saved_models[model]))
 
     def do_destroy(self, arg):
         """
         Usage: destroy BaseModel model-id
-        Deletes an instance based on the class name and id
+        Deletes an instance based on the class name an id
         """
 
         args = arg.split() if arg else [False]
@@ -71,39 +72,26 @@ class HBNBCommand(cmd.Cmd):
             saved_models = storage.all()
             model = find_model(saved_models, id)
             if model:
-<<<<<<< HEAD
-                del self.storage[model.__class__.__name__ + '.' + id]
-                save_to_file()
-            else:
-                print("** no instance found **")
-=======
                 del saved_models[model]
                 storage.save()
                 return
->>>>>>> refs/remotes/origin/main
 
-    def do_all(self, arg):
+    def do_all(slef, arg):
         """
         Usage: all BaseModel or all
-        Prints all string representation of all instances
+        Prins all string representation of all instances
         """
 
         saved_models = storage.all()
 
-<<<<<<< HEAD
-        instances = get_all_instances(args)
-        for instance in instances:
-            print(instances)
-=======
         if arg:
             if is_valid_input(arg):
                 print(
-                    [f"{str(saved_models[model])}"
+                    [f"{to_str(saved_models[model])}"
                      for model in saved_models
                      if saved_models[model]['__class__'] == arg])
         else:
-            print([f"{str(saved_models[model])}" for model in saved_models])
->>>>>>> refs/remotes/origin/main
+            print([f"{to_str(saved_models[model])}" for model in saved_models])
 
     def do_update(self, arg):
         """
@@ -121,34 +109,15 @@ class HBNBCommand(cmd.Cmd):
 
             model = find_model(saved_models, id)
 
-<<<<<<< HEAD
-            if len(args) < 4:
-                print("** attribute name missing **")
-                return
-
-            if len(args) < 5:
-                print("** value missing **")
-                return
-
-            model = find_model(id)
             if model:
-                setattr(model, attribute_name, attribute_value)
-                model.save()
-            else:
-                print("** no instance found **")
-
-
-def is_valid_class(arg, id=True):
-=======
-            if model:
+                value = ''.join(x for x in value if x != '"')
                 saved_models[model][attr] = value
                 storage.save()
 
 
 def is_valid_input(arg, id=True, attribute=True, value=True):
->>>>>>> refs/remotes/origin/main
     """
-    Checks if the class is valid.
+    Checks if class is valid. 
     id is optional
     """
     if not arg:
@@ -170,44 +139,6 @@ def is_valid_input(arg, id=True, attribute=True, value=True):
     return True
 
 
-<<<<<<< HEAD
-def find_model(id):
-    """
-    Find stored models based on id
-    """
-    filename = "file.json"
-    with open(filename, 'r') as json_file:
-        saved_models = json.load(json_file)
-        for model in saved_models:
-            if model.split('.')[1] == id:
-                return saved_models[model]
-    return None
-
-
-def get_all_instances(args):
-    """
-    Get all instances based on class name or all
-    """
-    filename = "file.json"
-    with open(filename, 'r') as json_file:
-        saved_models = json.load(json_file)
-        instances = [str(saved_models[model]) for model in saved_models
-                     if not args or model.split('.')[0] == args[0]]
-        return instances
-
-
-def save_to_file():
-    """
-    Save instances to the file
-    """
-    filename = "file.json"
-    with open(filename, 'r') as json_file:
-        saved_models = json.load(json_file)
-        # Save instances to the file (implement this part according to your data structure)
-        # saved_models[new_instance.id] = new_instance
-    with open(filename, 'w') as json_file:
-        json.dump(saved_models, json_file)
-=======
 def find_model(models, id):
     for model in models:
         if model.split('.')[1] == id:
@@ -215,9 +146,12 @@ def find_model(models, id):
 
     print("** no instance found **")
     return False
->>>>>>> refs/remotes/origin/main
+
+
+def to_str(model):
+    if model['__class__'] == "BaseModel":
+        return BaseModel(**model)
 
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
