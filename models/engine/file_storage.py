3c7d94de19ef9f062d65bf520cac12c207a5bd36
TopBoyDrako <sinbadprince9@gmail.com>
2023-12-10 22:57:18 +0100

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,9 +3,9 @@
 This module defines a class FileStorage that serializes instances
 to a JSON file and deserializes JSON file to instances
 """
-
-
 import json
+import uuid
+import os
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
@@ -28,14 +28,12 @@ class FileStorage:
         """
         Returns the dictionary __objects
         """
-
         return self.__objects
 
     def new(self, obj):
         """
         sets in __objects the obj with key <obj class name>.id
         """
-
         key = f"{type(obj).__name__}.{obj.id}"
         self.__objects[key] = obj
 
@@ -43,7 +41,6 @@ class FileStorage:
         """
         serializes __objects to the JSON file (path: __file_path)
         """
-
         filename = self.__file_path
         with open(filename, 'w') as json_file:
             json.dump({key: value.to_dict()
