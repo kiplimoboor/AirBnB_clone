697afad3b2d4f2c7b259407d64353410acf43009
kiplimoboor <kiplimoboor@pm.me>
2023-12-11 01:32:37 +0300

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,6 +9,18 @@ import models
 from models import storage
 from models.base_model import BaseModel
 
+import os
+import models
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 filename = "file.json"
 
 
