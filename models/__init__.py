aa88a10be816195ccb32f84f4ef5ad0380383f4b
kiplimoboor <kiplimoboor@pm.me>
2023-12-10 23:42:56 +0300

    better docstrings

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" This is a magic method for the models directory"""
+    """
+    This module packages the models
+    """
 
 
 from models.engine.file_storage import FileStorage
-
 
 storage = FileStorage()
 storage.reload()
